43be1e91e2dd04517f4ff5854f899a8f248da5b5
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-07-05 22:58:31 -0500

    First commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            card
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { onMounted } from '@vue/runtime-core'
+import { useStore } from 'vuex'
+
+export default {
+    setup(){
+        const store = useStore()
+        onMounted(() => {
+            store.dispatch('getPaises')
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    paises: [],
+    paisesFiltrados: []
   },
   mutations: {
+    setPaises(state, payload){
+      state.paises = payload
+    },
+    setPaisesFiltrados(state, payload){
+      state.paisesFiltrados = payload
+    }
   },
   actions: {
+    async getPaises({commit}){
+      try {
+        const resp = await fetch('https://restcountries.eu/rest/v2/all')
+        const data = await resp.json()
+        // console.log(data)
+        commit('setPaises', data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
   modules: {
   }
